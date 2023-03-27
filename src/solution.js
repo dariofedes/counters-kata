02614c4ceaaa340d824b8e247d56f309d87b7d63
Dario Fernandez <dario.fernandez@promocionesfarma.com>
2023-03-27 18:03:12 +0200

    feat: ensure it increases a counter when commanded to do so

--- src/solution.js
+++ src/solution.js
@@ -1,3 +1,5 @@
 export function solution(n, a) {
+    if(a.length) return 1
+    
     return 0;
 }
--- test/solution.spec.js
+++ test/solution.spec.js
@@ -12,4 +12,16 @@ describe('solution', () => {
         // Then
         expect(result).toBe(0)
     });
+
+    test('should increase the counter by 1 when an increase operation is passed and n is 1', ()=> {
+        //Given
+        const n = 1;
+        const a = [1];
+
+        //When
+        const result = solution(n, a);
+        
+        //Then
+        expect(result).toBe(1);
+    })
 })
