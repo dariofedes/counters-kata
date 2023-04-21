7bf62e3dd03842a07f440532a24a51189fda4432
Lisandro Bertoli <lisandro.bertoli2@promocionesfarma.com>
2023-04-21 15:05:44 +0200

    feat: increase counter when counter number is 1

--- src/main/java/com/example/demo/Counter.java
+++ src/main/java/com/example/demo/Counter.java
@@ -1,2 +1,15 @@
-package com.example.demo;public class Counter {
+package com.example.demo;
+
+import java.util.List;
+
+public class Counter {
+
+    public Counter() {
+    }
+
+    public int[] solution(int numberOfCounters, int[] operations) {
+        var result = new int[numberOfCounters];
+        result[0] = operations.length;
+        return result;
+    }
 }
--- src/test/java/com/example/demo/CounterShould.java
+++ src/test/java/com/example/demo/CounterShould.java
@@ -1,11 +1,39 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
 
 class CounterShould {
 
-	@Test
-	void contextLoads() {
-	}
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 8})
+    void should_return_array_with_length_equal_to_the_counter_number(int numberOfCounters) {
+        var expectedLength = numberOfCounters;
 
+        var counter = new Counter();
+
+        var result = counter.solution(numberOfCounters, new int[0]);
+
+        Assertions.assertEquals(expectedLength, result.length);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "1", "1,1", "1,1,1", "1,1,1,1,1,1,1"})
+    void should_increase_the_counter_for_each_operation(String stringOperations) {
+        var numberOfCounters = 1;
+        var operations = Arrays.stream(stringOperations.split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        var expectedCounterValue = operations.length;
+        var counter = new Counter();
+
+        var result = counter.solution(numberOfCounters, operations);
+
+        var actualCounterValue = result[0];
+        Assertions.assertEquals(expectedCounterValue, actualCounterValue);
+    }
 }
